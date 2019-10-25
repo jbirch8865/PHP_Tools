8e492ef09e5e12c9b4cee73645348d23718eab8e
root <root@localhost.localdomain>
2019-10-25 14:31:47 -0700

    update

--- ClassLoader.php
+++ ClassLoader.php
@@ -5,12 +5,12 @@ require dirname(__FILE__) . DIRECTORY_SEPARATOR . "DockerClass.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "NumberValidator.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "SendSMS.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "ConfigFileClass.php";
+require dirname(__FILE__) . DIRECTORY_SEPARATOR . "StartupVariables.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "Database/ClassLoader.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "UserClass.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "TestClass.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "BootstrapHTMLClass.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "BootstrapJSClass.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "LoggingClass.php";
-require dirname(__FILE__) . DIRECTORY_SEPARATOR . "StartupVariables.php";
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . "SystemClass.php";
 ?>
