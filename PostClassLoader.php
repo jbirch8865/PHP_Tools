f54d1d6fff47739a062632762f34fae1fe2b81e6
root <root@localhost.localdomain>
2020-03-29 17:12:46 -0700

    added post scripts

--- PostClassLoader.php
+++ PostClassLoader.php
@@ -1,2 +1,13 @@
 <?php
+$toolbelt_base->Companies = new \DatabaseLink\Table('Companies',$toolbelt_base->dblink);
+$toolbelt_base->Programs = new \DatabaseLink\Table('Programs',$toolbelt_base->dblink);
+$toolbelt_base->Configs = new \DatabaseLink\Table('Configs',$toolbelt_base->dblink);
+$toolbelt_base->Company_Configs = new \DatabaseLink\Table('Company_Configs',$toolbelt_base->dblink);
+ADODB_Active_Record::TableHasMany('Companies','Company_Configs','company_id','\Company\Company_Config');
+ADODB_Active_Record::TableHasMany('Company_Configs','Configs','config_id','\Company\Config');
+$toolbelt_base->Users = new \DatabaseLink\Table('Users',$toolbelt_base->dblink);
+ADODB_Active_Record::TableKeyHasMany('Companies','id','Users','company_id','\Company\Company');
+ADODB_Active_Record::TableBelongsTo('Users','Companies','company_id','id','\Company\Company');
+$toolbelt_base->Programs_Have_Sessions = new \DatabaseLink\Table('Programs_Have_Sessions',$toolbelt_base->dblink);
+
 ?>
