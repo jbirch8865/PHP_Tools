1693abff49a2f94ed09f37875e0b92a0de03c5bd
root <root@localhost.localdomain>
2020-04-01 13:49:36 -0700

    added roles

---
+++ Deployment_Scripts/2Database_Schema/7CompanyRoles.php
@@ -0,0 +1,154 @@
+<?php declare(strict_types=1);
+
+$toolbelt_base->Company_Roles = new \DatabaseLink\Table('Company_Roles',$toolbelt_base->dblink);
+company_config_Validate_ID_Column($toolbelt_base->Company_Roles);
+company_config_Validate_Company_ID_Column($toolbelt_base->Company_Roles);
+company_config_Validate_Role_Name($toolbelt_base->Company_Roles);
+company_config_Validate_Active_Status_Column($toolbelt_base->Company_Roles);
+$toolbelt_base->Company_Roles->Load_Columns();
+ADODB_Active_Record::TableHasMany('Companies','Company_Roles','company_id','\Company\Company_Roles');
+function company_config_Validate_ID_Column(\DatabaseLink\Table $Company_Roles)
+{
+    if($column = $Company_Roles->Get_Column('id'))
+    {
+        if($column->Get_Column_Key() != "PRI")
+        {
+            $column->Set_Column_Key("PRI"); 
+        }
+        if($column->Get_Data_Type() != "int(11)")
+        {
+            $column->Set_Data_Type("int(11)");
+        }
+        if($column->Get_Default_Value() != null)
+        {
+            $column->Set_Default_Value(null);
+        }
+        if($column->Is_Column_Nullable())
+        {
+            $column->Column_Is_Not_Nullable();
+        }
+        if(!$column->Does_Auto_Increment())
+        {
+            $column->Column_Auto_Increments();
+        }
+    }else
+    {
+        $column = new \DatabaseLink\Column('id',$Company_Roles,array(
+            'COLUMN_TYPE' => 'int(11)',
+            'COLUMN_DEFAULT' => null,
+            'is_nullable' => false,
+            'column_key' => "PRI",
+            'EXTRA' => "AUTO_INCREMENT")
+        );
+    }
+}
+function company_config_Validate_Company_ID_Column(\DatabaseLink\Table $Company_Roles)
+{
+    if($column = $Company_Roles->Get_Column('company_id'))
+    {
+        if($column->Get_Column_Key() != "")
+        {
+            $column->Set_Column_Key(""); 
+        }
+        if($column->Get_Data_Type() != "int(11)")
+        {
+            $column->Set_Data_Type("int(11)");
+        }
+        if($column->Get_Default_Value() != null)
+        {
+            $column->Set_Default_Value(null);
+        }
+        if($column->Is_Column_Nullable())
+        {
+            $column->Column_Is_Not_Nullable();
+        }
+        if($column->Does_Auto_Increment())
+        {
+            $column->Column_Does_Not_Auto_Increments();
+        }
+        $column->Update_Column();
+    }else
+    {
+        $column = new \DatabaseLink\Column('company_id',$Company_Roles,array(
+            'COLUMN_TYPE' => 'int(11)',
+            'COLUMN_DEFAULT' => null,
+            'is_nullable' => false,
+            'column_key' => "",
+            'EXTRA' => "")
+        );
+    }
+}
+function company_config_Validate_Role_Name(\DatabaseLink\Table $Company_Roles)
+{
+    if($column = $Company_Roles->Get_Column('Role_Name'))
+    {
+        if($column->Get_Column_Key() != "")
+        {
+            $column->Set_Column_Key(""); 
+        }
+        if($column->Get_Data_Type() != "varchar(200)")
+        {
+            $column->Set_Data_Type("varchar(200)");
+        }
+        if($column->Get_Default_Value() != null)
+        {
+            $column->Set_Default_Value(null);
+        }
+        if($column->Is_Column_Nullable())
+        {
+            $column->Column_Is_Not_Nullable();
+        }
+        if($column->Does_Auto_Increment())
+        {
+            $column->Column_Does_Not_Auto_Increments();
+        }
+        $column->Update_Column();
+    }else
+    {
+        $column = new \DatabaseLink\Column('Role_Name',$Company_Roles,array(
+            'COLUMN_TYPE' => 'varchar(200)',
+            'COLUMN_DEFAULT' => null,
+            'is_nullable' => false,
+            'column_key' => "",
+            'EXTRA' => "")
+        );
+    }
+}
+function company_config_Validate_Active_Status_Column(\DatabaseLink\Table $Company_Roles)
+{
+    global $cConfigs;
+    if($column = $Company_Roles->Get_Column('active_status'))
+    {
+        if($column->Get_Column_Key() != "")
+        {
+            $column->Set_Column_Key(""); 
+        }
+        if($column->Get_Data_Type() != "INT(11)")
+        {
+            $column->Set_Data_Type("INT(11)");
+        }
+        if($column->Get_Default_Value() != "1")
+        {
+            $column->Set_Default_Value("1");
+        }
+        if($column->Is_Column_Nullable())
+        {
+            $column->Column_Is_Not_Nullable();
+        }
+        if($column->Does_Auto_Increment())
+        {
+            $column->Column_Does_Not_Auto_Increments();
+        }
+        $column->Update_Column();
+    }else
+    {
+        $column = new \DatabaseLink\Column('active_status',$Company_Roles,array(
+            'COLUMN_TYPE' => 'INT(11)',
+            'COLUMN_DEFAULT' => "1",
+            'is_nullable' => false,
+            'column_key' => "",
+            'EXTRA' => "")
+        );
+    }
+}
+?>
